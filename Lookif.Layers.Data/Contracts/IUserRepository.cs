4295df08bcd2aa442fe4e6ecd29f0f3ef0ffacc9
Farshad HN <sfarshadhn@outlook.com>
2021-07-25 12:28:17 +0430

    Init

---
+++ Lookif.Layers.Data/Contracts/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Lookif.Layers.Core.MainCore.Identities;
+using System.Threading;
+using System.Threading.Tasks; 
+
+namespace Lookif.Layers.Data.Contracts
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetByUserAndPass(string username, string password, CancellationToken cancellationToken);
+
+        Task AddAsync(User user, string password, CancellationToken cancellationToken);
+        Task UpdateSecuirtyStampAsync(User user, CancellationToken cancellationToken);
+        Task UpdateLastLoginDateAsync(User user, CancellationToken cancellationToken);
+    }
+}
---
+++ Lookif.Layers.Data/Repositories/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using Lookif.Layers.Data.Contracts;
+using Lookif.Layers.Core.MainCore.Identities;
+using Lookif.Library.Common;
+using Lookif.Library.Common.Exceptions;
+using Lookif.Library.Common.Utilities;
+using Microsoft.EntityFrameworkCore;
+using System; 
+using System.Linq; 
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lookif.Layers.Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository, IScopedDependency
+    {
+        public UserRepository(ApplicationDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public Task<User> GetByUserAndPass(string username, string password, CancellationToken cancellationToken)
+        {
+            var passwordHash = SecurityHelper.GetSha256Hash(password);
+            return Table.Where(p => p.UserName == username && p.PasswordHash == passwordHash).SingleOrDefaultAsync(cancellationToken);
+        }
+
+        public Task UpdateSecuirtyStampAsync(User user, CancellationToken cancellationToken)
+        {
+            //user.SecurityStamp = Guid.NewGuid();
+            return UpdateAsync(user, cancellationToken);
+        }
+
+        //public override void Update(User entity, bool saveNow = true)
+        //{
+        //    entity.SecurityStamp = Guid.NewGuid();
+        //    base.Update(entity, saveNow);
+        //}
+
+        public Task UpdateLastLoginDateAsync(User user, CancellationToken cancellationToken)
+        {
+            user.LastLoginDate = DateTimeOffset.Now;
+            return UpdateAsync(user, cancellationToken);
+        }
+
+        public async Task AddAsync(User user, string password, CancellationToken cancellationToken)
+        {
+            var exists = await TableNoTracking.AnyAsync(p => p.UserName == user.UserName);
+            if (exists)
+                throw new BadRequestException("نام کاربری تکراری است");
+
+            var passwordHash = SecurityHelper.GetSha256Hash(password);
+            user.PasswordHash = passwordHash;
+            await base.AddAsync(user, cancellationToken);
+        }
+    }
+}
