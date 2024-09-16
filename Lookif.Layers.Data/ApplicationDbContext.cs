56ede4ff2218a55e8d52d2f6f1d091817dd51695
Farshad <sfarshadhn@gmail.com>
2024-09-16 14:07:38 +0330

    Migrate to .Net 8

--- Lookif.Layers.Data/ApplicationDbContext.cs
+++ Lookif.Layers.Data/ApplicationDbContext.cs
@@ -12,126 +12,125 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace Lookif.Layers.Data
+namespace Lookif.Layers.Data;
+
+public abstract class ApplicationDbContext : IdentityDbContext<User, Role, Guid>
 {
-    public abstract class ApplicationDbContext : IdentityDbContext<User, Role, Guid>
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions options)
-            : base(options)
-        {
-        }
+    }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-        }
-        public static Assembly CoreLayerAssembly { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+    public static Assembly CoreLayerAssembly { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
 
-            modelBuilder.RegisterAllEntities<IEntity,ITemporal>(CoreLayerAssembly);
-            modelBuilder.RegisterAllEntities<IEntity<Guid>,ITemporal>(CoreLayerAssembly);
-            modelBuilder.RegisterEntityTypeConfiguration(CoreLayerAssembly);
-            modelBuilder.AddRestrictDeleteBehaviorConvention();
-            modelBuilder.AddSequentialGuidForIdConvention();
-            modelBuilder.AddPluralizingTableNameConvention();
-        }
+        modelBuilder.RegisterAllEntities<IEntity, ITemporal>(CoreLayerAssembly);
+        modelBuilder.RegisterAllEntities<IEntity<Guid>, ITemporal>(CoreLayerAssembly);
+        modelBuilder.RegisterEntityTypeConfiguration(CoreLayerAssembly);
+        modelBuilder.AddRestrictDeleteBehaviorConvention();
+        modelBuilder.AddSequentialGuidForIdConvention();
+        modelBuilder.AddPluralizingTableNameConvention();
+    }
 
-        public override int SaveChanges()
-        {
-            _cleanString();
-            return base.SaveChanges();
-        }
+    public override int SaveChanges()
+    {
+        _cleanString();
+        return base.SaveChanges();
+    }
 
-        public override int SaveChanges(bool acceptAllChangesOnSuccess)
-        {
-            _cleanString();
-            return base.SaveChanges(acceptAllChangesOnSuccess);
-        }
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        _cleanString();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
 
-        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
-        {
-            _cleanString();
-            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-        }
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        _cleanString();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
 
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+
+        _cleanString();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    private void SoftDelete()
+    {
+        foreach (var entry in ChangeTracker.Entries())
         {
-            
-            _cleanString();
-            return base.SaveChangesAsync(cancellationToken);
-        }
-        private void SoftDelete()
-        {
-            foreach (var entry in ChangeTracker.Entries())
+            switch (entry.State)
             {
-                switch (entry.State)
+                //case EntityState.Added:
+                //    entry.CurrentValues["IsDeleted"] = false;
+                //    // iterate over each nav. prop to performe cascade soft delete = false
+
+                //    break;
+
+                case EntityState.Deleted:
+                    entry.State = EntityState.Modified;
+                    entry.CurrentValues["IsDeleted"] = true;
+                    // iterate over each nav. prop to performe cascade soft delete = true
+                    IterateThroughNavigationProperties(entry);
+                    break;
+            }
+        }
+    }
+    private void IterateThroughNavigationProperties(EntityEntry entry)
+    {
+        foreach (var navigationEntry in entry.Navigations.Where(n => !((INavigation)n.Metadata).IsOnDependent))
+        {
+            if (navigationEntry is CollectionEntry collectionEntry)
+            {
+                foreach (var dependentEntry in collectionEntry.CurrentValue)
                 {
-                    //case EntityState.Added:
-                    //    entry.CurrentValues["IsDeleted"] = false;
-                    //    // iterate over each nav. prop to performe cascade soft delete = false
-
-                    //    break;
-
-                    case EntityState.Deleted:
-                        entry.State = EntityState.Modified;
-                        entry.CurrentValues["IsDeleted"] = true;
-                        // iterate over each nav. prop to performe cascade soft delete = true
-                        IterateThroughNavigationProperties(entry);
-                        break;
+                    HandleDependent(Entry(dependentEntry));
+                }
+            }
+            else
+            {
+                var dependentEntry = navigationEntry.CurrentValue;
+                if (dependentEntry != null)
+                {
+                    HandleDependent(Entry(dependentEntry));
                 }
             }
         }
-        private void IterateThroughNavigationProperties(EntityEntry entry)
+    }
+    private void HandleDependent(EntityEntry entry)
+    {
+        entry.CurrentValues["IsDeleted"] = true;
+    }
+    private void _cleanString()
+    {
+        var changedEntities = ChangeTracker.Entries()
+            .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+        foreach (var item in changedEntities)
         {
-            foreach (var navigationEntry in entry.Navigations.Where(n => !((INavigation)n.Metadata).IsOnDependent))
+            if (item.Entity == null)
+                continue;
+
+            var properties = item.Entity.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.CanRead && p.CanWrite && p.PropertyType == typeof(string));
+
+            foreach (var property in properties)
             {
-                if (navigationEntry is CollectionEntry collectionEntry)
-                {
-                    foreach (var dependentEntry in collectionEntry.CurrentValue)
-                    {
-                        HandleDependent(Entry(dependentEntry));
-                    }
-                }
-                else
-                {
-                    var dependentEntry = navigationEntry.CurrentValue;
-                    if (dependentEntry != null)
-                    {
-                        HandleDependent(Entry(dependentEntry));
-                    }
-                }
-            }
-        }
-        private void HandleDependent(EntityEntry entry)
-        {
-            entry.CurrentValues["IsDeleted"] = true;
-        }
-        private void _cleanString()
-        {
-            var changedEntities = ChangeTracker.Entries()
-                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
-            foreach (var item in changedEntities)
-            {
-                if (item.Entity == null)
-                    continue;
+                var val = (string)property.GetValue(item.Entity, null);
 
-                var properties = item.Entity.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    .Where(p => p.CanRead && p.CanWrite && p.PropertyType == typeof(string));
-
-                foreach (var property in properties)
+                if (val.HasValue())
                 {
-                    var val = (string)property.GetValue(item.Entity, null);
-
-                    if (val.HasValue())
-                    {
-                        var newVal = val.Fa2En().FixPersianChars();
-                        if (newVal == val)
-                            continue;
-                        property.SetValue(item.Entity, newVal, null);
-                    }
+                    var newVal = val.Fa2En().FixPersianChars();
+                    if (newVal == val)
+                        continue;
+                    property.SetValue(item.Entity, newVal, null);
                 }
             }
         }
--- Lookif.Layers.Data/Repositories/Repository.cs
+++ Lookif.Layers.Data/Repositories/Repository.cs
@@ -10,237 +10,236 @@ using Lookif.Library.Common.Utilities;
 using Lookif.Layers.Core.Infrastructure.Base.Repositories;
 using Lookif.Layers.Core.Infrastructure;
 
-namespace Lookif.Layers.Data.Repositories
-{
+namespace Lookif.Layers.Data.Repositories;
 
-    public class Repository<TEntity> : Repository<TEntity, Guid>, IRepository<TEntity> where TEntity : class, IEntity<Guid>
+
+public class Repository<TEntity> : Repository<TEntity, Guid>, IRepository<TEntity> where TEntity : class, IEntity<Guid>
+{
+    public Repository(ApplicationDbContext dbContext) : base(dbContext)
     {
-        public Repository(ApplicationDbContext dbContext) : base(dbContext)
-        {
-        }
+    }
+
+}
+
+
+
+public class Repository<TEntity, Tkey> : IRepository<TEntity, Tkey> where TEntity : class, IEntity<Tkey>
+{
+    protected readonly ApplicationDbContext DbContext;
+    public DbSet<TEntity> Entities { get; }
+    public virtual IQueryable<TEntity> Table => Entities;
+    public virtual IQueryable<TEntity> TableNoTracking => Entities.AsNoTracking();
+
+    public Repository(ApplicationDbContext dbContext)
+    {
+        DbContext = dbContext;
+        Entities = DbContext.Set<TEntity>(); // City => Cities 
+    }
+
+    #region Async Method
+    public virtual async ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids)
+    {
+        return await Entities.FindAsync(ids, cancellationToken);
+
 
     }
 
+    public virtual IQueryable<TEntity> GetTemporal<Temporal>() where Temporal : ITemporal, TEntity
+        => Entities.TemporalAll<TEntity>();
 
+    public virtual async Task<List<TEntity>> GetTemporal<Temporal>(CancellationToken cancellationToken) where Temporal : ITemporal, TEntity
+        => await Entities.TemporalAll<TEntity>().ToListAsync(cancellationToken);
 
-    public class Repository<TEntity, Tkey> : IRepository<TEntity, Tkey> where TEntity : class, IEntity<Tkey>
+    public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
     {
-        protected readonly ApplicationDbContext DbContext;
-        public DbSet<TEntity> Entities { get; }
-        public virtual IQueryable<TEntity> Table => Entities;
-        public virtual IQueryable<TEntity> TableNoTracking => Entities.AsNoTracking();
+        Assert.NotNull(entity, nameof(entity));
+        await Entities.AddAsync(entity, cancellationToken).ConfigureAwait(false);
+        if (saveNow)
+            await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
 
-        public Repository(ApplicationDbContext dbContext)
-        {
-            DbContext = dbContext;
-            Entities = DbContext.Set<TEntity>(); // City => Cities 
-        }
+    }
 
-        #region Async Method
-        public virtual async ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids)
-        {
-            return await Entities.FindAsync(ids, cancellationToken);
+    public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        await Entities.AddRangeAsync(entities, cancellationToken).ConfigureAwait(false);
+        if (saveNow)
+            await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
 
+    }
 
-        }
-
-        public virtual IQueryable<TEntity> GetTemporal<Temporal>() where Temporal : ITemporal, TEntity
-            => Entities.TemporalAll<TEntity>();
-
-        public virtual async Task<List<TEntity>> GetTemporal<Temporal>(CancellationToken cancellationToken) where Temporal : ITemporal, TEntity
-            => await Entities.TemporalAll<TEntity>().ToListAsync(cancellationToken);
-
-        public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
-        {
+    public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
+    {
             Assert.NotNull(entity, nameof(entity));
-            await Entities.AddAsync(entity, cancellationToken).ConfigureAwait(false);
-            if (saveNow)
-                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-        }
-
-        public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
-        {
-            Assert.NotNull(entities, nameof(entities));
-            await Entities.AddRangeAsync(entities, cancellationToken).ConfigureAwait(false);
-            if (saveNow)
-                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-        }
-
-        public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
-        {
-                Assert.NotNull(entity, nameof(entity));
-                Detach(entity);
-                ChangeState(entity, EntityState.Modified);
-                Entities.Update(entity);
-                if (saveNow)
-                    await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-         
-        }
-
-        public virtual async Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
-        {
-            Assert.NotNull(entities, nameof(entities));
-            Entities.UpdateRange(entities);
-            if (saveNow)
-                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-        }
-
-        public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
-        {
-            Assert.NotNull(entity, nameof(entity));
-            Entities.Remove(entity);
-            if (saveNow)
-                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-        }
-
-        public virtual async Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
-        {
-            Assert.NotNull(entities, nameof(entities));
-            Entities.RemoveRange(entities);
-            if (saveNow)
-                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-        }
-        #endregion
-
-        #region Sync Methods
-        public virtual TEntity GetById(params object[] ids)
-        {
-            return Entities.Find(ids);
-        }
-
-        public virtual void Add(TEntity entity, bool saveNow = true)
-        {
-            Assert.NotNull(entity, nameof(entity));
-            Entities.Add(entity);
-            if (saveNow)
-                DbContext.SaveChanges();
-        }
-
-        public virtual void AddRange(IEnumerable<TEntity> entities, bool saveNow = true)
-        {
-            Assert.NotNull(entities, nameof(entities));
-            Entities.AddRange(entities);
-            if (saveNow)
-                DbContext.SaveChanges();
-        }
-
-        public virtual void Update(TEntity entity, bool saveNow = true)
-        {
-            Assert.NotNull(entity, nameof(entity));
+            Detach(entity);
             ChangeState(entity, EntityState.Modified);
             Entities.Update(entity);
             if (saveNow)
-                DbContext.SaveChanges();
-        }
-
-        public virtual void UpdateRange(IEnumerable<TEntity> entities, bool saveNow = true)
-        {
-            Assert.NotNull(entities, nameof(entities));
-            Entities.UpdateRange(entities);
-            if (saveNow)
-                DbContext.SaveChanges();
-        }
-
-        public virtual void Delete(TEntity entity, bool saveNow = true)
-        {
-            Assert.NotNull(entity, nameof(entity));
-            Entities.Remove(entity);
-            if (saveNow)
-                DbContext.SaveChanges();
-        }
-
-        public virtual void DeleteRange(IEnumerable<TEntity> entities, bool saveNow = true)
-        {
-            Assert.NotNull(entities, nameof(entities));
-            Entities.RemoveRange(entities);
-            if (saveNow)
-                DbContext.SaveChanges();
-        }
-        #endregion
-
-        #region Attach & Detach
-        private  void ChangeState(TEntity entity, EntityState entityState)
-        {
-            Assert.NotNull(entity, nameof(entity));
-            var entry = DbContext.Entry(entity);
-            if (entry != null && entry.State != EntityState.Detached)
-                entry.State = entityState;
-
-            var local = DbContext.Set<TEntity>().Local.FirstOrDefault(entry => entry.Id.Equals(entity.Id));
-            // check if local is not null 
-            if (local != null)
-            {
-                // detach
-                DbContext.Entry(local).State = entityState;
-            }
-        }
-
-
-        public virtual void Detach(TEntity entity)
-        {
-            Assert.NotNull(entity, nameof(entity));
-            var entry = DbContext.Entry(entity);
-            if (entry != null)
-                entry.State = EntityState.Detached;
-
-            var local = DbContext.Set<TEntity>().Local.FirstOrDefault(entry => entry.Id.Equals(entity.Id));
-            // check if local is not null 
-            if (local != null)
-            {
-                // detach
-                DbContext.Entry(local).State = EntityState.Detached;
-            }
-        }
-
-        public virtual void Attach(TEntity entity)
-        {
-            Assert.NotNull(entity, nameof(entity));
-            if (DbContext.Entry(entity).State == EntityState.Detached)
-                Entities.Attach(entity);
-        }
-        #endregion
-
-        #region Explicit Loading
-        public virtual async Task LoadCollectionAsync<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty, CancellationToken cancellationToken)
-            where TProperty : class
-        {
-            Attach(entity);
-
-            var collection = DbContext.Entry(entity).Collection(collectionProperty);
-            if (!collection.IsLoaded)
-                await collection.LoadAsync(cancellationToken).ConfigureAwait(false);
-        }
-
-        public virtual void LoadCollection<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty)
-            where TProperty : class
-        {
-            Attach(entity);
-            var collection = DbContext.Entry(entity).Collection(collectionProperty);
-            if (!collection.IsLoaded)
-                collection.Load();
-        }
-
-        public virtual async Task LoadReferenceAsync<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty, CancellationToken cancellationToken)
-            where TProperty : class
-        {
-            Attach(entity);
-            var reference = DbContext.Entry(entity).Reference(referenceProperty);
-            if (!reference.IsLoaded)
-                await reference.LoadAsync(cancellationToken).ConfigureAwait(false);
-        }
-
-        public virtual void LoadReference<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty)
-            where TProperty : class
-        {
-            Attach(entity);
-            var reference = DbContext.Entry(entity).Reference(referenceProperty);
-            if (!reference.IsLoaded)
-                reference.Load();
-        }
-
-
-        #endregion
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+     
     }
+
+    public virtual async Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        Entities.UpdateRange(entities);
+        if (saveNow)
+            await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        Entities.Remove(entity);
+        if (saveNow)
+            await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public virtual async Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        Entities.RemoveRange(entities);
+        if (saveNow)
+            await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+    #endregion
+
+    #region Sync Methods
+    public virtual TEntity GetById(params object[] ids)
+    {
+        return Entities.Find(ids);
+    }
+
+    public virtual void Add(TEntity entity, bool saveNow = true)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        Entities.Add(entity);
+        if (saveNow)
+            DbContext.SaveChanges();
+    }
+
+    public virtual void AddRange(IEnumerable<TEntity> entities, bool saveNow = true)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        Entities.AddRange(entities);
+        if (saveNow)
+            DbContext.SaveChanges();
+    }
+
+    public virtual void Update(TEntity entity, bool saveNow = true)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        ChangeState(entity, EntityState.Modified);
+        Entities.Update(entity);
+        if (saveNow)
+            DbContext.SaveChanges();
+    }
+
+    public virtual void UpdateRange(IEnumerable<TEntity> entities, bool saveNow = true)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        Entities.UpdateRange(entities);
+        if (saveNow)
+            DbContext.SaveChanges();
+    }
+
+    public virtual void Delete(TEntity entity, bool saveNow = true)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        Entities.Remove(entity);
+        if (saveNow)
+            DbContext.SaveChanges();
+    }
+
+    public virtual void DeleteRange(IEnumerable<TEntity> entities, bool saveNow = true)
+    {
+        Assert.NotNull(entities, nameof(entities));
+        Entities.RemoveRange(entities);
+        if (saveNow)
+            DbContext.SaveChanges();
+    }
+    #endregion
+
+    #region Attach & Detach
+    private  void ChangeState(TEntity entity, EntityState entityState)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        var entry = DbContext.Entry(entity);
+        if (entry != null && entry.State != EntityState.Detached)
+            entry.State = entityState;
+
+        var local = DbContext.Set<TEntity>().Local.FirstOrDefault(entry => entry.Id.Equals(entity.Id));
+        // check if local is not null 
+        if (local != null)
+        {
+            // detach
+            DbContext.Entry(local).State = entityState;
+        }
+    }
+
+
+    public virtual void Detach(TEntity entity)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        var entry = DbContext.Entry(entity);
+        if (entry != null)
+            entry.State = EntityState.Detached;
+
+        var local = DbContext.Set<TEntity>().Local.FirstOrDefault(entry => entry.Id.Equals(entity.Id));
+        // check if local is not null 
+        if (local != null)
+        {
+            // detach
+            DbContext.Entry(local).State = EntityState.Detached;
+        }
+    }
+
+    public virtual void Attach(TEntity entity)
+    {
+        Assert.NotNull(entity, nameof(entity));
+        if (DbContext.Entry(entity).State == EntityState.Detached)
+            Entities.Attach(entity);
+    }
+    #endregion
+
+    #region Explicit Loading
+    public virtual async Task LoadCollectionAsync<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty, CancellationToken cancellationToken)
+        where TProperty : class
+    {
+        Attach(entity);
+
+        var collection = DbContext.Entry(entity).Collection(collectionProperty);
+        if (!collection.IsLoaded)
+            await collection.LoadAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public virtual void LoadCollection<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> collectionProperty)
+        where TProperty : class
+    {
+        Attach(entity);
+        var collection = DbContext.Entry(entity).Collection(collectionProperty);
+        if (!collection.IsLoaded)
+            collection.Load();
+    }
+
+    public virtual async Task LoadReferenceAsync<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty, CancellationToken cancellationToken)
+        where TProperty : class
+    {
+        Attach(entity);
+        var reference = DbContext.Entry(entity).Reference(referenceProperty);
+        if (!reference.IsLoaded)
+            await reference.LoadAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    public virtual void LoadReference<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> referenceProperty)
+        where TProperty : class
+    {
+        Attach(entity);
+        var reference = DbContext.Entry(entity).Reference(referenceProperty);
+        if (!reference.IsLoaded)
+            reference.Load();
+    }
+
+
+    #endregion
 }
